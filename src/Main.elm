5114df2f04f3ddbae1f3bd6d9f6258c3afa95ea3
Thomas Mohme <2261413+tmohme@users.noreply.github.com>
2020-03-06 17:37:19 +0100

    . . . with dynamic Video list view

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,15 @@ import Browser
 import Css exposing (absolute, position, px, right, top)
 import Html.Styled exposing (Html, div, h1, h3, img, p, text, toUnstyled)
 import Html.Styled.Attributes exposing (css, src)
+import List exposing (map)
+
+
+type alias Video =
+    { id : Int
+    , title : String
+    , speaker : String
+    , videoUrl : String
+    }
 
 
 type alias Model =
@@ -14,17 +23,26 @@ type Msg
     = Msg
 
 
+unwatchedVideos =
+    [ Video 1 "Building and breaking things" "John Doe" "https://youtu.be/PsaFVLr8t4E"
+    , Video 2 "The development process" "Jane Smith" "https://youtu.be/PsaFVLr8t4E"
+    , Video 3 "The Web 7.0" "Matt Miller" "https://youtu.be/PsaFVLr8t4E"
+    ]
+
+
+watchedVideos =
+    [ Video 4 "Mouseless development" "Tom Jerry" "https://youtu.be/PsaFVLr8t4E" ]
+
+
 view : Model -> Html Msg
 view _ =
     div []
         [ h1 [] [ text "KotlinConf Explorer" ]
         , div []
             [ h3 [] [ text "Videos to watch" ]
-            , p [] [ text "John Doe: Building and breaking things" ]
-            , p [] [ text "Jane Smith: The development process" ]
-            , p [] [ text "Matt Miller: The Web 7.0" ]
-            , h3 [] [ text "Videos to watched" ]
-            , p [] [ text "Tom Jerry: Mouseless development" ]
+            , div [] <| map (\v -> p [] [ text v.title ]) unwatchedVideos
+            , h3 [] [ text "Videos watched" ]
+            , div [] <| map (\v -> p [] [ text v.title ]) watchedVideos
             ]
         , div [ css [ position absolute, top (px 10), right (px 10) ] ]
             [ h3 [] [ text "John Doe: Building and breaking things" ]
